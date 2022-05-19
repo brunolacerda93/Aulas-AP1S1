8e568fb63559a0177b4ead9d0d039bef78afef63
Bruno Lacerda <bruno.lacerda9393@gmail.com>
2022-05-19 16:07:10 -0300

    Update em Projeto 1

--- LojaAndromeda.py
+++ LojaAndromeda.py
@@ -27,6 +27,8 @@ def menu(x):
             return 0
         elif opc >= '1' and opc <= '5':
             subMenu(opc, x)
+        elif opc == 'cc': del clientes[len(clientes)-1] # Limpeza para quando um elemento "None" é adicionado na lista
+        elif opc == 'cp': del produtos[len(produtos)-1] # Limpeza para quando um elemento "None" é adicionado na lista
 
 # Opções do submenu
 def subMenu(opc, x):
@@ -38,9 +40,9 @@ def subMenu(opc, x):
         case '3':
             incluir(x); system('pause')
         case '4':
-            system('pause')
+            alterar(x); system('pause')
         case '5':
-            system('pause')
+            excluir(x); system('pause')
         case _:
             return 0
 
@@ -63,7 +65,9 @@ def incluir(x):
 # Formulário para adicionar Clientes
 def formCliente():
     form = []
-    cpf = input("CPF: "); form.append(cpf)
+    cpf = input("CPF: "); 
+    if percorreCliente(cpf) == -1: form.append(cpf)                         # Teste para checar se o cpf já
+    else: print("Já existe cliente com esse CPF"); return form.clear()      # consta na lista de clientes
     nome = input("Nome: "); form.append(nome)
     data = input("Data de Nascimento: "); form.append(data)
     sexo = input("Sexo: "); form.append(sexo)
@@ -75,13 +79,111 @@ def formCliente():
 #Formulário para adicionar Produtos
 def formProduto():
     form = []
-    cod = input("Código: "); form.append(cod)
+    cod = input("Código: "); 
+    if percorreProduto(cod) == -1: form.append(cod)                         # Teste para checar se o código
+    else: print("Já existe produto com esse código"); return form.clear()   # já consta na lista de produtos
     descr = input("Descrição: "); form.append(descr)
     peso = input("Peso: "); form.append(peso)
     preco = input("Preço: R$"); form.append(preco)
     desconto = input("Desconto: "); form.append(desconto)
     data = input("Data de Validade: "); form.append(data)
     return form
+
+#Função para alterar elementos
+def alterar(x):
+    if x == '1':
+        cpf = input("Digite o CPF: ")
+        cliente = percorreCliente(cpf) 
+        if cliente == -1:
+            print("Não existe cliente com esse CPF")
+        else:
+            mostraCliente(cliente)
+            while True:
+                system('cls')
+                print(clientes[cliente])
+                print("\nO que deseja alterar:")
+                print(" 1 - Nome")
+                print(" 2 - Data de Nascimento")
+                print(" 3 - Sexo")
+                print(" 4 - Salário")
+                print(" 5 - E-mail")
+                print(" 6 - Telefones")
+                print(" 0 - Retornar")
+                opc = input("Escolha: ")
+                if opc == '0':
+                    return 0
+                elif opc >= '1' and opc <= '6':
+                    write(clientes, cliente, opc)
+
+    elif x == '2':
+        cod = input("Digite o código: ")
+        produto = percorreProduto(cod)
+        if produto == -1:
+            print("Não existe produto com esse código")
+        else:
+            mostraProduto(produto)
+            while True:
+                system('cls')
+                print(produtos[produto])
+                print("\nO que deseja alterar:")
+                print(" 1 - Descrição")
+                print(" 2 - Peso")
+                print(" 3 - Preço")
+                print(" 4 - Desconto")
+                print(" 5 - Data de Validade")
+                print(" 0 - Retornar")
+                opc = input("Escolha: ")
+                if opc == '0':
+                    return 0
+                elif opc >= '1' and opc <= '5':
+                    write(produtos, produto, opc)
+
+#Função para sobrescrever elementos de uma lista global
+def write(lista, i, j):
+    novo = input("Digite o novo: ")
+    lista[i][int(j)] = novo
+
+# Função para percorrer clientes em busca de um CPF igual
+def percorreCliente(cpf):
+    for i in range(len(clientes)):
+        if clientes[i][0] == cpf:
+            return i
+    return -1
+
+# Função para exibir o conteúdo da lista de Clientes
+def mostraCliente(cliente):
+    print(clientes[cliente])
+
+# Função para percorrer produtos em busca de um código igual
+def percorreProduto(cod):
+    for i in range(len(produtos)):
+        if produtos[i][0] == cod:
+            return i
+    return -1
+
+# Função para exibir o conteúdo da lista de Produtos
+def mostraProduto(produto):
+    print(produtos[produto])
+
+# Função para excluir um elemento de uma lista
+def excluir(x):
+    if x == '1':
+        cpf = input("Digite o CPF do cliente que deseja excluir: ")
+        cliente = percorreCliente(cpf)
+        if cliente == -1: print("Não existe cliente com esse CPF")
+        else:
+            mostraCliente(cliente)
+            opc = input("Deseja remover este cliente do sistema da loja? [s/S]")
+            if opc == 's' or opc == 'S': del clientes[cliente]; print("Removido!")
+
+    elif x == '2':
+        cod = input("Digite o código do produto que deseja excluir: ")
+        produto = percorreProduto(cod)
+        if produto == -1: print("Não existe produto com esse código")
+        else:
+            mostraProduto(produto)
+            opc = input("Deseja remover este produto do sistema da loja? [s/S]")
+            if opc == 's' or opc == 'S': del produtos[produto]; print("Removido!")
 
 # Declaração da função main()
 def main():
@@ -95,7 +197,7 @@ def main():
         opc = input("Escolha: ")
         if opc == '0':
             break
-        else:
+        elif opc == '1' or opc == '2':
             system('cls')
             menu(opc)
 
